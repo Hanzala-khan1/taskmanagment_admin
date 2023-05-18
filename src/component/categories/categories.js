86f139c16fd13df0f6642e1a1c79b1a4e73adafe
hunny-nik <106017090+hunny-nik@users.noreply.github.com>
2023-05-18 11:03:51 +0500

    my new code

--- src/component/categories/categories.js
+++ src/component/categories/categories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './categories.css'
 import Lower_nav from '../shared/Lower_nav'
 import Nav from '../shared/Nav'
+
 import Filter from '../shared/Filter'
 import globe from '../../assets/svg/globe.png'
 import Delete from '../../assets/svg/delete.png'
@@ -11,17 +12,57 @@ const Categories = () => {
     return (
         <div>
             <div>
-                <div> <Nav /> </div>
-                <div>
-                    <Lower_nav />
-                    <Filter />
-                </div>
-                <div>
-                    <h1>my frist commit </h1>
+                <Nav />
+            </div>
+            <div>
+                <Lower_nav />
+                <Filter />
+            </div>
+            <div>
+                <div className='categories'>
+
+                    <div className='top-x'>
+                        <h4>Categories</h4>
+                        <Category_popUp />
+                    </div>
+                    <div style={{ marginRight: '6rem' }}>
+                        <div className='col-lg-3 col-md-6 col-sm-12 d-flex category'>
+                            <div>
+                                <img src={globe} />
+                            </div>
+                            <div className='top-gap'>
+                                <h5>Web Development</h5>
+                                <div>
+                                    <img src={Delete} /> <a>Delete</a> <img src={Edit} /> <a>Edit</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-lg-3 col-md-6 col-sm-12 d-flex category'>
+                            <div>
+                                <img src={globe} />
+                            </div>
+                            <div className='top-gap'>
+                                <h5>Flutter</h5>
+                                <div>
+                                    <img src={Delete} /> <a>Delete</a> <img src={Edit} /> <a>Edit</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-lg-3 col-md-6 col-sm-12 d-flex category'>
+                            <div>
+                                <img src={globe} />
+                            </div>
+                            <div className='top-gap'>
+                                <h5>Android Development</h5>
+                                <div>
+                                    <img src={Delete} /> <a>Delete</a> <img src={Edit} /> <a>Edit</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     )
-}
-
+};
 export default Categories
